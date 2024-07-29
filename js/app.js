7cadf2c3b9985cf7e8c1c1841fb80c2325a81ed1
Sergey <92174566+SergeyKakulin@users.noreply.github.com>
2024-07-29 22:58:11 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,60 +14,60 @@ document.querySelector('#play').addEventListener('click', function(e){
 });
 
 document.querySelector('#play2').addEventListener('click', function(e){
-    if(e.target.value === '1') {
+    if(e.target.value === '1 8 2') {
       e.target.value = '▶';
       audio.src = e.target.getAttribute('data-url');
       audio.play();
     } else {
-      e.target.value = '1';
+      e.target.value = '1 8 2';
       audio.currentTime = 0;
       audio.pause();
     }
   });
 
   document.querySelector('#play3').addEventListener('click', function(e){
-    if(e.target.value === '2') {
+    if(e.target.value === '5 9 8') {
       e.target.value = '▶';
       audio.src = e.target.getAttribute('data-url');
       audio.play();
     } else {
-      e.target.value = '2';
+      e.target.value = '5 9 8';
       audio.currentTime = 0;
       audio.pause();
     }
   });
 
   document.querySelector('#play4').addEventListener('click', function(e){
-    if(e.target.value === '3') {
+    if(e.target.value === '7 3 4') {
       e.target.value = '▶';
       audio.src = e.target.getAttribute('data-url');
       audio.play();
     } else {
-      e.target.value = '3';
+      e.target.value = '7 3 4';
       audio.currentTime = 0;
       audio.pause();
     }
   });
 
   document.querySelector('#play5').addEventListener('click', function(e){
-    if(e.target.value === '4') {
+    if(e.target.value === '6 2 1') {
       e.target.value = '▶';
       audio.src = e.target.getAttribute('data-url');
       audio.play();
     } else {
-      e.target.value = '4';
+      e.target.value = '6 2 1';
       audio.currentTime = 0;
       audio.pause();
     }
   });
 
   document.querySelector('#play6').addEventListener('click', function(e){
-    if(e.target.value === '5') {
+    if(e.target.value === '1 1 9') {
       e.target.value = '▶';
       audio.src = e.target.getAttribute('data-url');
       audio.play();
     } else {
-      e.target.value = '5';
+      e.target.value = '1 1 9';
       audio.currentTime = 0;
       audio.pause();
     }
